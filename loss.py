Fixed syntaxic errors, and added normalization back

loss.py
@@ -13,17 +13,15 @@ class LossNetwork(nn.Module):
         style_layers = ["relu1_2", "relu2_2", "relu3_3", "relu4_3"]
         content_layers = ["relu2_2"]
 
-        style_layers.sort()
-
         vgg = vgg16(weights="DEFAULT").features.eval().to(DEVICE)
         vgg.requires_grad_(False)
 
         self.style_losses = []
         self.content_losses = []
 
-        # self.normalize = Normalize()
-        # self.model = nn.Sequential(self.normalize)
-        self.model = nn.Sequential()
+        self.normalize = Normalize()
+        self.model = nn.Sequential(self.normalize)
+        # self.model = nn.Sequential()
 
         pool_idx, conv_idx, relu_idx, bn_idx = (1, 1, 1, 1)
         style_loss_idx, content_loss_idx = (1, 1)
@@ -59,7 +57,9 @@ class LossNetwork(nn.Module):
             if name in style_layers:
                 target_style = self.model(style).detach()
                 style_loss = StyleLoss(target_style)
-                self.model.add_module(f"style_loss_{style_loss_idx}", style_loss)
+                self.model.add_module(
+                    f"style_loss_{style_loss_idx}", style_loss
+                )
                 self.style_losses.append(style_loss)
                 style_loss_idx += 1
 
@@ -68,7 +68,9 @@ class LossNetwork(nn.Module):
                 content_loss = ContentLoss(
                     target_content_dummy, copy.deepcopy(self.model)
                 )
-                self.model.add_module(f"content_loss_{content_loss_idx}", content_loss)
+                self.model.add_module(
+                    f"content_loss_{content_loss_idx}", content_loss
+                )
                 self.content_losses.append(content_loss)
                 content_loss_idx += 1
 
@@ -104,7 +106,9 @@ class Normalize(nn.Module):
 def compute_gram_matrix(inputs):
     batch_size, cnn_channels, height, width = inputs.size()
     features = inputs.view(batch_size, cnn_channels, height * width)
-    return features.bmm(features.transpose(1, 2)).div(cnn_channels * height * width)
+    return features.bmm(features.transpose(1, 2)).div(
+        cnn_channels * height * width
+    )
 
 
 class ContentLoss(nn.Module):
@@ -133,6 +137,10 @@ class StyleLoss(nn.Module):
 
 
 def calculate_total_variation_loss(inputs):
-    tv_height = torch.sum(torch.square(inputs[:, :, 1:, :] - inputs[:, :, :-1, :]))
-    tv_width = torch.sum(torch.square(inputs[:, :, :, 1:] - inputs[:, :, :, :-1]))
+    tv_height = torch.sum(
+        torch.square(inputs[:, :, 1:, :] - inputs[:, :, :-1, :])
+    )
+    tv_width = torch.sum(
+        torch.square(inputs[:, :, :, 1:] - inputs[:, :, :, :-1])
+    )
     return tv_width + tv_height

model.py
@@ -8,13 +8,34 @@ class TransformationNetwork(nn.Module):
         kernel = 3
         self.num_residual_blocks = 5
         self.convolutional_downsampling = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_9, stride=1, padding=kernel_9 // 2, padding_mode="reflect"),
+            nn.Conv2d(
+                3,
+                32,
+                kernel_9,
+                stride=1,
+                padding=kernel_9 // 2,
+                padding_mode="reflect",
+            ),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel, stride=2, padding=kernel // 2, padding_mode="reflect"),
+            nn.Conv2d(
+                32,
+                64,
+                kernel,
+                stride=2,
+                padding=kernel // 2,
+                padding_mode="reflect",
+            ),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel, stride=2, padding=kernel // 2, padding_mode="reflect"),
+            nn.Conv2d(
+                64,
+                128,
+                kernel,
+                stride=2,
+                padding=kernel // 2,
+                padding_mode="reflect",
+            ),
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
@@ -26,10 +47,19 @@ class TransformationNetwork(nn.Module):
             nn.BatchNorm2d(128),
         )
         self.convolutional_upsampling = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel, stride=2, padding=kernel // 2, output_padding=1),
+            nn.ConvTranspose2d(
+                128,
+                64,
+                kernel,
+                stride=2,
+                padding=kernel // 2,
+                output_padding=1,
+            ),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel, stride=2, padding=kernel // 2, output_padding=1),
+            nn.ConvTranspose2d(
+                64, 32, kernel, stride=2, padding=kernel // 2, output_padding=1
+            ),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 3, kernel_9, stride=1, padding=kernel_9 // 2),
@@ -38,10 +68,10 @@ class TransformationNetwork(nn.Module):
         )
 
     def forward(self, x):
-        inputs = self.convolutional_downsampling(x)
+        x = self.convolutional_downsampling(x)
         for _ in range(self.num_residual_blocks):
-            x = self.residual_block(inputs)
-            x += inputs
-            inputs = x
-        x = self.convolutional_upsampling(inputs)
+            residual = x
+            x = self.residual_block(x)
+            x += residual
+        x = self.convolutional_upsampling(x)
         return x